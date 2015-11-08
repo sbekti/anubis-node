f57c4dd4da64f049b2419755dfd55ccadc00a18c
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-11-07 23:36:40 -0800

    Add MQTT and GPIO functionalities

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  ANUBIS_MQTT_SERVER: process.env.ANUBIS_MQTT_SERVER,
+  ANUBIS_NODE_TOPIC: process.env.ANUBIS_MQTT_TOPIC,
+  ANUBIS_GPIO_PIN: process.env.ANUBIS_GPIO_PIN
+};
+
+export default config;
---
+++ server/index.js
@@ -0,0 +1,50 @@
+import mqtt from 'mqtt';
+import gpio from 'pi-gpio';
+import config from '../config';
+
+function processMessage(topic, message) {
+  if (topic == config.ANUBIS_NODE_TOPIC) {
+    try {
+      const payload = JSON.parse(message);
+      const state = payload.state;
+
+      if ((state == 0) || (state == 1)) {
+        gpio.write(config.ANUBIS_GPIO_PIN, state, (err) => {
+          if (err) {
+            console.error('Failed to set GPIO');
+            return;
+          }
+
+          console.log('GPIO set to: ' + state);
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+function openMQTTConnection() {
+  const client = mqtt.connect(config.ANUBIS_MQTT_SERVER);
+
+  client.on('connect', () => {
+    client.subscribe(config.ANUBIS_NODE_TOPIC);
+  });
+
+  client.on('message', (topic, message) => {
+    processMessage(topic.toString(), message.toString());
+  });
+}
+
+function openGPIO() {
+  gpio.open(config.ANUBIS_GPIO_PIN, 'output', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    openMQTTConnection();
+  });
+}
+
+openGPIO();
